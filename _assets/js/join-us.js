abc0e7a37444adcfac407ef4e78827aec1c0aa05
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-07-31 14:53:49 +0100

    - step by step mobile

--- _assets/js/join-us.js
+++ _assets/js/join-us.js
@@ -16,6 +16,11 @@ $(function () {
 
     $("#step1-title").addClass("gray");
     $("#step2-title").removeClass("gray");
+
+    $("#m-step-form-img1-active, #m-step1-title").addClass("d-none");
+    $("#m-step-form-img2-active, #m-step2-title").removeClass("d-none");
+    $("#m-dots").removeClass("dots-right");
+    $("#m-dots").addClass("dots-full");
   });
 
   $("#submit-step2").click(function () {
@@ -35,6 +40,12 @@ $(function () {
 
     $("#step2-title").addClass("gray");
     $("#step3-title").removeClass("gray");
+
+    $("#m-step-form-img2-active, #m-step2-title").addClass("d-none");
+    $("#m-step-form-img3-active, #m-step3-title").removeClass("d-none");
+
+    $("#m-dots").removeClass("dots-full");
+    $("#m-dots").addClass("dots-left");
   });
 
   $("#back-step2").click(function () {
@@ -54,6 +65,12 @@ $(function () {
 
     $("#step1-title").removeClass("gray");
     $("#step2-title").addClass("gray");
+
+    $("#m-step-form-img1-active, #m-step1-title").removeClass("d-none");
+    $("#m-step-form-img2-active, #m-step2-title").addClass("d-none");
+
+    $("#m-dots").removeClass("dots-full");
+    $("#m-dots").addClass("dots-right");
   });
 
   $("#submit-step3").click(function () {});
