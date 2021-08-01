5f7f9a608608f7785f2c90e4f020120a4e4540ed
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-08-01 03:01:46 +0100

    - add latest news scroll (solution page)

--- _assets/js/solutions.js
+++ _assets/js/solutions.js
@@ -3,3 +3,9 @@ $(".jcarousel").imgScroll({
   btn02: ".scroll-btn-right",
   step: 400,
 });
+
+$(".jcarousel-latest-news").imgScroll({
+  btn01: ".scroll-btn-left-latest-news",
+  btn02: ".scroll-btn-right-latest-news",
+  step: 370,
+});
